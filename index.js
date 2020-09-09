947667146605d477167325c802789e1db36ddd2e
Kristen Sumiko Jones <kristensumiko@gmail.com>
2020-09-09 14:56:25 -0400

    first submit ready

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
-'use strict';
+(function () {
+  'use strict';
 
-function getRepos() {
-    fetch('https://api.github.com/users/' + `${$("#js-username").val()}` + '/repos?sort=created')
-    .then(response => response.json())
-    .then(responseJson => 
-      displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'+error));
-}
-
-function displayResults(responseJson) {
-  console.log(responseJson);
-  //replace the existing image with the new one
-  if (responseJson.message === 'Not Found') {
-    $('#js-error-message').html(
-      `<p>${responseJson.message}</p>
-      <p>TRY ANOTHER USERNAME</p>`
-    );
+  function getRepos() {
+      fetch('https://api.github.com/users/' + $("#js-username").val() + '/repos?sort=created')
+      .then(response => response.json())
+      .then(responseJson => 
+        displayResults(responseJson))
+      .catch(error => alert('Something went wrong. Try again later.'+error));
   }
-  else {
-  $('.results').html(generateResultsList(responseJson))}
-  //display the results section
-  $('#results').removeClass('hidden');
-}
 
-function generateResultsList(responseJson) {
-    let resultsHtml = ''
-    let userName = '${responseJson.value[i].name}'
-    let repoLink = '${responseJson.value[i].html_url}'
-    for(let i=0; i<responseJson.length; i++) {
-        console.log(responseJson[i]);
-      resultsHtml += `<li><h3><p>${userName}</p></h3>
-      <a href="${repoLink}">${repoLink}</a>
-      </li>`;
+  function displayResults(responseJson) {
+    console.log(responseJson);
+    if (responseJson.message === 'Not Found') {
+      $('#js-error-message').html(
+        `<p>${responseJson.message}</p>
+        <p>TRY ANOTHER USERNAME</p>`
+      );
+      $('#results').addClass('hidden');
     }
-    return resultsHtml;
-    console.log(resultsHtml)
+    else {
+      $('#results-list').html(generateResultsList(responseJson));
+      $('#results').removeClass('hidden');
+    }
   }
 
-function watchForm() {
-  $('form').submit(event => {
-    event.preventDefault();
-    getRepos();
-  });
-}
+  function generateResultsList(responseJson) {
+      let resultsHtml = ''
+      for(let i=0; i<responseJson.length; i++) {
+          console.log(responseJson[i]);
+        resultsHtml += `<li><h3><p>${responseJson[i].full_name}</p></h3>
+        <a href="${responseJson[i].html_url}">${responseJson[i].html_url}</a>
+        </li>`;
+      }
+      return resultsHtml;
+    }
 
-$(function() {
-  console.log('App loaded! Waiting for submit!');
-  watchForm();
-});
+  function watchForm() {
+    $('form').submit(event => {
+      event.preventDefault();
+      getRepos();
+    });
+  }
+
+  $(function() {
+    console.log('App loaded! Waiting for submit!');
+    watchForm();
+  });
+}());
